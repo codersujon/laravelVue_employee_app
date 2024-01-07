ef0c068ca7cde8ab67a34035dcc033b36ceeef03
MD SUJAN AHMED <csesujon155@gmail.com>
2024-01-08 02:24:21 +0600

    push upto controller

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+use App\Models\Employee;
+// use Illuminate\view\view;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $employees = Employee::all();
+        return response()->json($employees);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $employee = Employee::create([
+            "name"=> $request->name,
+            "address"=> $request->address,
+            "mobile_no"=> $request->mobile_no,
+        ]);
+        $employee->save();
+        return response()->json("Employee Created!");
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $employee = Employee::find($id);
+        return response()->json($employee);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $employee = Employee::find($id);
+        $employee->update($request->all());
+        return response()->json("Employee Updated!");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $employee = Employee::find($id);
+        $employee->delete();
+        return response()->json("Employee Deleted!");
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+/**
+ * EmployeeController
+ */
+
+ Route::controller(EmployeeController::class)->group(function(){
+    Route::get('/employees', 'index');
+    Route::post('/save', 'store');
+    Route::put('/update/{id}', 'update');
+    Route::delete('/delete/{id}', 'destroy');
+});
